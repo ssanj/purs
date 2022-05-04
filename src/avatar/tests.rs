4d6aa3e455054a686b4677bbe12f6c63df6b0818
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-05-05 09:28:15 +1000

    Add more tests

--- src/avatar/tests.rs
+++ src/avatar/tests.rs
@@ -1,11 +1,12 @@
+use dirs::cache_dir;
 use wiremock::{MockServer, Mock, ResponseTemplate};
 use wiremock::matchers::{method, path};
-use crate::avatar::{get_url_data, does_cache_file_exist};
+use crate::avatar::{get_url_data, does_cache_file_exist, get_or_create_avatar_file};
 use crate::model::{Url, AvatarCacheFile, CacheFileStatus, UserId};
 
 use tempfile::tempdir;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Write, Read};
 
 
 #[tokio::test]
@@ -56,4 +57,37 @@ async fn test_does_cache_file_exist_with_file() {
   cache_dir.close().expect("Could not close temp dir");
 
   assert_eq!(result.unwrap(), CacheFileStatus::Exists);
+}
+
+
+#[tokio::test]
+async fn test_get_or_create_avatar_file() {
+    let mock_server = MockServer::start().await;
+    let data = "1234".as_bytes();
+    let template =
+      ResponseTemplate::new(200)
+        .set_body_bytes(data);
+
+    Mock::given(method("GET"))
+        .and(path("/u/12345"))
+        .respond_with(template)
+        // Mounting the mock on the mock server - it's now effective!
+        .mount(&mock_server)
+        .await;
+
+    let file_name = "12345.png";
+    let cache_dir = tempdir().unwrap().into_path().to_string_lossy().to_string();
+    let avatar_url = Url::new(format!("{}/u/12345?v=4", &mock_server.uri()));
+    let user_id = UserId::new(12345);
+    let file_url = get_or_create_avatar_file(&user_id, &avatar_url, &cache_dir).await.unwrap();
+    let expected_file_url = format!("file://{}/{}", cache_dir, file_name);
+
+    assert_eq!(file_url.to_string(), expected_file_url);
+
+    let cache_file_path = expected_file_url.replace("file://", "");
+    let mut file = File::open(&cache_file_path).expect(&format!("could not find file: {}", &cache_file_path));
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents).unwrap();
+
+    assert_eq!(file_contents, std::str::from_utf8(data).unwrap());
 }
